e71118675b18118c1e8b7a15cac08fa75db9c139
fiscal cliff <why@not.com>
2018-03-22 18:33:02 +0300

    qualified

--- lib/sequel/extensions/batches.rb
+++ lib/sequel/extensions/batches.rb
@@ -10,23 +10,30 @@ module Sequel
         pk ||= self.db.schema(first_source)
                  .select{|r| r[1][:primary_key]}
                  .map(&:first) or raise MissingPKError
+        qualified_pk = pk.map { |c| Sequel[first_source][c] }
 
-        pk_expr = pk.map do |col|
-          Sequel.as(
-            Sequel.pg_array(
-              [
-                Sequel.function(:min, col),
-                Sequel.function(:max, col)
-              ]
-            ), :"#{col}"
-          )
-        end
+        pk_expr = (-> (pk:) do
+          pk.map do |col|
+            colname = col.is_a?(Symbol) ? col : col.column
+            Sequel.as(
+              Sequel.pg_array(
+                [
+                  Sequel.function(:min, col),
+                  Sequel.function(:max, col)
+                ]
+              ), :"#{colname}"
+            )
+          end
+        end)
 
-        entire_min_max = self.order(*pk).select(*pk_expr).first
+        entire_min_max = self.order(*pk).select(*pk_expr.call(pk: qualified_pk)).first
         min_max = {}
 
         range_expr =  (-> (col, range) do
-          Sequel.&(Sequel.expr(col) >= range[0], Sequel.expr(col) <= range[1])
+          Sequel.&(
+            Sequel.expr(col) >= range[0],
+            Sequel.expr(col) <= range[1],
+          )
         end)
 
         loop do
@@ -35,14 +42,14 @@ module Sequel
             entire_min_max[col][1] = finish[col] || entire_min_max[col][1]
           end
 
-          ds = self.order(*pk).limit(of).where(
+          ds = self.order(*qualified_pk).limit(of).where(
             Sequel.&(*pk.map { |col| range_expr.call(col, entire_min_max[col]) })
           )
           if min_max.present?
-            ds=ds.where(Sequel.function(:concat, pk.map{|col| Sequel.cast(col, :text)}) > Sequel.function(:concat, min_max.values.map(&:last)))
+            ds=ds.where(Sequel.function(:concat, qualified_pk.map{|col| Sequel.cast(col, :text)}) > Sequel.function(:concat, min_max.values.map(&:last)))
           end
 
-          min_max = self.db.from(ds).select(*pk_expr).first
+          min_max = self.db.from(ds).select(*pk_expr.call(pk: pk)).first
 
           break if min_max.values.flatten.any?(&:blank?)
           yield self.where(Sequel.&(*pk.map { |col| range_expr.call(col, min_max[col]) }))
