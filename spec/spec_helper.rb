f8a99e9294c0bc12aeed0120466a8efdc1cf0437
fiscal cliff <why@not.com>
2018-03-28 09:32:06 +0300

    right coverage info

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require "bundler/setup"
-require "sequel/extensions/batches"
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
