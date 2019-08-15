13dc2cc4b86ac5602140de36da5d797bf7c1f5b4
Yuri Smirnov <tycooon@yandex.ru>
2019-08-15 17:50:56 +0300

    Fix infinite loops (#1)
    * add logger + failing spec
    
    * rework
    
    * add actual_finish to prevent infinite batches
    
    * make ds updateable
    
    * refactor a bit
    
    * optimize some stuff + more specs
    
    * fix typo
    
    * add spec for nulls case

--- Gemfile
+++ Gemfile
@@ -1,6 +1,14 @@
 source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-
 # Specify your gem's dependencies in sequel-batches.gemspec
 gemspec
+
+# MRI/Rubinius Adapter Dependencies
+platforms :ruby do
+  gem "pg"
+end
+
+# JRuby Adapter Dependencies
+platforms :jruby do
+  gem "jdbc-postgres"
+end
--- lib/sequel/extensions/batches.rb
+++ lib/sequel/extensions/batches.rb
@@ -6,62 +6,67 @@ module Sequel
     module Batches
       MissingPKError = Class.new(StandardError)
 
-      def in_batches(pk: nil, of: 1000, start: {}, finish: {})
-        pk ||= self.db.schema(first_source)
-                 .select{|r| r[1][:primary_key]}
-                 .map(&:first) or raise MissingPKError
-        qualified_pk = pk.map { |c| Sequel[first_source][c] }
+      def in_batches(pk: nil, of: 1000, start: nil, finish: nil)
+        pk ||=
+          db.schema(first_source)
+            .select { |x| x[1][:primary_key] }
+            .map(&:first) or raise MissingPKError
 
-        pk_expr = (-> (pk:) do
-          pk.map do |col|
-            colname = col.is_a?(Symbol) ? col : col.column
-            Sequel.as(
-              Sequel.pg_array(
-                [
-                  Sequel.function(:min, col),
-                  Sequel.function(:max, col)
-                ]
-              ), :"#{colname}"
-            )
+        qualified_pk = pk.map { |x| Sequel[first_source][x] }
+        pk_combinations = pk.map.with_index { |x, i| pk[0..i] }
+
+        # For composite PK (x, y, z) this will generate the following WHERE expression:
+        # (x > ?) OR (x = ? AND y > ?) OR (x = ? AND y = ? AND z > ?)
+        conditions = lambda do |values, mode: :start, including: true|
+          or_conditions = pk_combinations.map do |keys|
+            and_conditions = keys.map.with_index do |key, index|
+              value = values.fetch(key)
+
+              # All conditions should use equality except for the last one
+              if index == keys.size - 1
+                mode == :finish ? Sequel[key] < value : Sequel[key] > value
+              else
+                Sequel[key] =~ value
+              end
+            end
+
+            and_conditions.reduce(:&)
           end
-        end)
 
-        entire_min_max = self.order(*pk).select(*pk_expr.call(pk: qualified_pk)).first
-        min_max = {}
+          if including
+            including_expr = pk.map { |key| Sequel[key] =~ values.fetch(key) }.reduce(:&)
+            or_conditions << including_expr
+          end
 
-        range_expr =  (-> (col, range) do
-          Sequel.&(
-            Sequel.expr(Sequel[first_source][col]) >= range[0],
-            Sequel.expr(Sequel[first_source][col]) <= range[1],
-          )
-        end)
+          or_conditions.reduce(:|)
+        end
+
+        base_ds = order(*qualified_pk)
+        base_ds = base_ds.where(conditions.call(start)) if start
+        base_ds = base_ds.where(conditions.call(finish, mode: :finish)) if finish
+
+        pk_ds = db.from(base_ds).select(*pk).order(*pk)
+        actual_start = pk_ds.first
+        actual_finish = pk_ds.last
+
+        return unless actual_start && actual_finish
+
+        base_ds = base_ds.where(conditions.call(actual_start))
+        base_ds = base_ds.where(conditions.call(actual_finish, mode: :finish))
+
+        current_instance = nil
 
         loop do
-          pk.each do |col|
-            entire_min_max[col][0] = start[col] || entire_min_max[col][0]
-            entire_min_max[col][1] = finish[col] || entire_min_max[col][1]
+          if current_instance
+            working_ds = base_ds.where(conditions.call(current_instance.to_h, including: false))
+          else
+            working_ds = base_ds
           end
 
-          ds = self.order(*qualified_pk).limit(of).where(
-            Sequel.&(*pk.map { |col| range_expr.call(col, entire_min_max[col]) })
-          )
-          if min_max.present?
-            pk_combinations = pk.each_with_index.map { |x, i| pk[0..-i] }
-            ds = ds.where(Sequel.|(*pk_combinations.each_with_index.map do |pks, i|
-              Sequel.&(*pks.each_with_index.map do |col, j|
-                if j == i
-                  Sequel[first_source][col] > min_max[col].last
-                else
-                  Sequel[first_source][col] >= min_max[col].last
-                end
-              end)
-            end))
-          end
+          current_instance = db.from(working_ds.limit(of)).select(*pk).order(*pk).last or break
+          working_ds = working_ds.where(conditions.call(current_instance.to_h, mode: :finish))
 
-          min_max = self.db.from(ds).select(*pk_expr.call(pk: pk)).first
-
-          break if min_max.values.flatten.any?(&:blank?)
-          yield self.where(Sequel.&(*pk.map { |col| range_expr.call(col, min_max[col]) }))
+          yield working_ds
         end
       end
 
