b3512d416058c95444354c88d8063da7007a4380
frvade <1924461+frvade@users.noreply.github.com>
2021-11-06 13:46:40 +0300

    fix rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ require "simplecov-lcov"
 
 SimpleCov::Formatter::LcovFormatter.config do |c|
   c.report_with_single_file = true
-  c.single_report_path = 'coverage/lcov.info'
+  c.single_report_path = "coverage/lcov.info"
 end
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
