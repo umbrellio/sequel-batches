fbaf9e70b6961564bf953b54e940367c8aa9f2f9
frvade <1924461+frvade@users.noreply.github.com>
2021-11-06 13:43:35 +0300

    fix jruby version fix lcov file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,10 @@ end
 require "simplecov"
 require "simplecov-lcov"
 
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   SimpleCov::Formatter::LcovFormatter,
